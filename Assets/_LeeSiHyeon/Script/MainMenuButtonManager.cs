ed6d6cc83404cbd76a710c8e2e81190a39bd7c07
LSH005 <isihyeon005shingu@gmail.com>
2026-06-08 18:07:37 +0900

    [feat] 버튼에 연결할 모든 Scene으로 가는 함수 구현

--- Assets/_LeeSiHyeon/Script/MainMenuButtonManager.cs
+++ Assets/_LeeSiHyeon/Script/MainMenuButtonManager.cs
@@ -2,33 +2,15 @@ using UnityEngine;
 
 public class MainMenuButtonManager : MonoBehaviour
 {
-    public void GoToSettingScene()
-    {
-        SceneTransition.Instance.TransitionToScene("02_Setting");
-    }
+    public void GoToSettingScene() => SceneTransition.Instance.TransitionToScene("02_Setting");
 
-    public void GoToQuestScene()
-    {
-        // 03_Quest 씬으로 이동하는 로직
-    }
+    public void GoToQuestScene() => SceneTransition.Instance.TransitionToScene("03_Quest");
 
-    public void GoToHuntingGroundScene()
-    {
-        // 04_HuntingGround 씬으로 이동하는 로직
-    }
+    public void GoToHuntingGroundScene() => SceneTransition.Instance.TransitionToScene("04_HuntingGround");
 
-    public void GoToCharacterScene()
-    {
-        // 05_Character 씬으로 이동하는 로직
-    }
+    public void GoToCharacterScene() => SceneTransition.Instance.TransitionToScene("05_Character");
 
-    public void GoToMenuScene()
-    {
-        // 06_Menu 씬으로 이동하는 로직
-    }
+    public void GoToMenuScene() => SceneTransition.Instance.TransitionToScene("06_Menu");
 
-    public void GoToTransactionScene()
-    {
-        // 07_Transaction 씬으로 이동하는 로직
-    }
+    public void GoToTransactionScene() => SceneTransition.Instance.TransitionToScene("07_Transaction");
 }
