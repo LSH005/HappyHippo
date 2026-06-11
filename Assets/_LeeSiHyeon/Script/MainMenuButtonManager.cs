ae7a0eccf846fce33837d5e7ad234ebde918f739
jinjooyoung <jjy26297449@gmail.com>
2026-06-11 18:12:45 +0900

    [Feat] 메인화면->메뉴씬 버튼 추가, 메뉴씬 씬전환 함수 연결

--- Assets/_LeeSiHyeon/Script/MainMenuButtonManager.cs
+++ Assets/_LeeSiHyeon/Script/MainMenuButtonManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MainMenuButtonManager : MonoBehaviour
 {
+    public void GoToMainScene() => SceneTransition.Instance.TransitionToScene("01_Main");
+
     public void GoToSettingScene() => SceneTransition.Instance.TransitionToScene("02_Setting");
 
     public void GoToQuestScene() => SceneTransition.Instance.TransitionToScene("03_Quest");
@@ -13,4 +15,13 @@ public class MainMenuButtonManager : MonoBehaviour
     public void GoToMenuScene() => SceneTransition.Instance.TransitionToScene("06_Menu");
 
     public void GoToTransactionScene() => SceneTransition.Instance.TransitionToScene("07_Transaction");
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
 }
