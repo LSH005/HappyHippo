870304a7ab765d5536a63fdfb1bf3085b304343b
blocks2094 <blocks2094@g.shingu.ac.kr>
2026-06-17 22:59:40 +0900

    [Feat] 04사냥터화면 설명 팝업창 추가

---
+++ Assets/_LeeHyunWoo/Scripts/PopupClose.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace LeeHyunWoo
+{
+    public class PopupClose : MonoBehaviour
+    {
+        [Header("비활성화할 팝업 UI")]
+        [SerializeField] private GameObject popupUI;
+
+        [Header("ESC 키로 닫기")]
+        [SerializeField] private bool closeWithEscape = true;
+
+        private void Awake()
+        {
+            if (popupUI == null)
+                popupUI = gameObject;
+        }
+
+        private void Update()
+        {
+            if (!closeWithEscape)
+                return;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ClosePopup();
+            }
+        }
+
+        public void ClosePopup()
+        {
+            if (popupUI != null)
+                popupUI.SetActive(false);
+        }
+
+        public void OpenPopup()
+        {
+            if (popupUI != null)
+                popupUI.SetActive(true);
+        }
+
+        public void TogglePopup()
+        {
+            if (popupUI != null)
+                popupUI.SetActive(!popupUI.activeSelf);
+        }
+    }
+}
